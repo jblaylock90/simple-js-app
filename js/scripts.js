81d87f8da408571c596d8d8dc526d8defec03939
jblaylock90 <grey90stanggt@gmail.com>
2022-06-18 22:09:21 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 alert('Hello world');
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+
 var favoriteFood = 'Mexican food!';
 document.write(favoriteFood);
