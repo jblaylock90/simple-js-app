bdab3b9e616da431a678642580a1259fa48bcaa0
jblaylock90 <grey90stanggt@gmail.com>
2022-06-19 21:52:41 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,4 @@ let pokemonList = [
   {name:'Bulbasaur', height: 0.7 m, type:['grass','poison']}
   {name:'Weedle', height: 0.3 m, type:['bug','poison'}
   {name: 'Nidoqueen', height: 1.3 m, type:['ground','poison'}
-];
-  document.write(pokemonList.name);
+]
