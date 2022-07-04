1917bca3b8591d00bc39309d910188af41d612bd
jblaylock90 <grey90stanggt@gmail.com>
2022-07-03 23:24:44 -0600

    changed if/else to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
-let pokemonList = [
-  {
-    name:'Bulbasaur',
-    height: 0.7,
-    type:['grass','poison']
-  },
-  {
-    name:'Weedle',
-    height: 0.3,
-    type:['bug','poison']
-  },
-  {
-    name: 'Nidoqueen',
-    height: 1.3,
-    type:['ground','poison']
-  }
-];
+let pokemonRepository = (function(){
+  let pokemonList = [
+    {
+      name:'Bulbasaur',
+      height: 0.7,
+      type:['grass','poison']
+    },
+    {
+      name:'Weedle',
+      height: 0.3,
+      type:['bug','poison']
+    },
+    {
+      name: 'Nidoqueen',
+      height: 1.3,
+      type:['ground','poison']
+    }
+  ];
 
-  for (let i = 0; i < pokemonList.length; i++){
-    if(pokemonList[i].height < 1){
-    document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + '<br>');
-  }else if (pokemonList[i].height>= 1) {
-    document.write(pokemonList[i].name + "'s height is " + pokemonList[i].height + ". Wow, that's Big!" + '<br>')
+  pokemonList.forEach(function(pokemonList){
+    document.write(pokemonList.name + "'s height is " + pokemonList.height + '<br>');
+  });
+
+  function add(pokemon){
+    pokemonList.push(pokemon);
   }
-}
+
+  function getAll(){
+    return pokemonList;
+  }
+
+  return{
+    add:add,
+    getAll: getAll
+  };
+
+})();
